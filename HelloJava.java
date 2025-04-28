facbef7d9571d934ce95abb0c91967c49d34756a
Kajal Kajal (Nokia) <kajal.kajal@nokia.com>
2025-04-28 11:19:22 +0530

    added HelloJava Program

---
+++ HelloJava.java
@@ -0,0 +1,8 @@
+public class HelloJava {
+    public static void main(String[] args) {
+        // Using a for loop to print "Hello Java" 5 times
+        for (int i = 1; i <= 5; i++) {
+            System.out.println("Hello Java");
+        }
+    }
+}
